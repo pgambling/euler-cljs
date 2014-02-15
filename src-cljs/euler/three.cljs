6d2b0a6a8d74649b554e73d1ea1aa4b40957272a
Phil Gambling <pgambling@gmail.com>
2014-02-14 22:40:11 -0600

    checkpoint

--- src-cljs/euler/three.cljs
+++ src-cljs/euler/three.cljs
@@ -3,13 +3,19 @@
 (defn gcd [a b]
   (if (zero? b)
     a
-    (gcd b (mod a b))))
+    (recur b (mod a b))))
+
+(defn prime-numbers [a n]
+  (let [n1 (inc n)
+        a1 (+ a (gcd n1 a))]
+    (cons (- a1 a) (lazy-seq (prime-numbers a1 n1)))))
+
+
+(defn multiple? [n div]
+  (= 0 (mod n div)))
 
 (defn largest-prime-factor [n]
-  (loop [i (Math/floor (Math/sqrt n))]
-    (if (and (= 0 (mod n i) (= 1 (gcd n i))))
-      i
-      (recur (dec i)))))
+  (take-while #(> % (/ n %)) (filter #(and (not= % 1) (multiple? n %)) (prime-numbers 7 1))))
 
 (defn run []
   (largest-prime-factor 600851475143))
